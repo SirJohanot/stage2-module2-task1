99710098313441dd2710bc24f08b82c62f0d537a
SirJohanot <pavelkrafft@gmail.com>
2023-03-26 17:25:56 +0300

    completed the task

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,37 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddUserServlet extends HttpServlet {
+
+    private static final String ADD_PAGE_PATH = "/jsp/add.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(ADD_PAGE_PATH)
+                .forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+
+        User newUser = new User(firstName, lastName);
+
+        Warehouse.getInstance()
+                .addUser(newUser);
+
+        req.setAttribute("user", newUser);
+
+        req.getRequestDispatcher(ADD_PAGE_PATH)
+                .forward(req, resp);
+    }
 }
